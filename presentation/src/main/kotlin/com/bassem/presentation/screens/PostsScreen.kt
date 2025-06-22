b94d9f0202613dd9eb84c390a27c78a576334103
basemomar94 <basemomar2012@gmail.com>
2025-06-22 10:46:18 +0300

    feat: Integrate ViewModel with PostsScreen

--- presentation/src/main/kotlin/com/bassem/presentation/screens/PostsScreen.kt
+++ presentation/src/main/kotlin/com/bassem/presentation/screens/PostsScreen.kt
@@ -1,12 +1,28 @@
 package com.bassem.presentation.screens
 
+import android.widget.Toast
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.bassem.presentation.PostsState
+import com.bassem.presentation.PostsViewModel
 import com.bassem.presentation.models.RedditPost
 
 
 @Composable
-fun PostsScreen(onPostClick: (RedditPost) -> Unit) {
+fun PostsScreen(viewModel: PostsViewModel = hiltViewModel(), onPostClick: (RedditPost) -> Unit) {
+    val state = viewModel.postsState.collectAsState().value
+    when (state) {
+        is PostsState.Loading -> {}
+        is PostsState.Success -> {
+            Toast.makeText(LocalContext.current, "Success ${state.posts.size}", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        is PostsState.Error -> {}
+    }
     Text(text = "Hello Screen")
 
 }
