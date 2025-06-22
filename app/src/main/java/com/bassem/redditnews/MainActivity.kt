bf630acc44c0ec393964160e93dcf29e3f2bc3d7
basemomar94 <basemomar2012@gmail.com>
2025-06-22 20:22:09 +0300

    feat: Enable Hilt for MainActivity
    This commit annotates the `MainActivity` class with `@AndroidEntryPoint`.
    
    This annotation enables field injection by Hilt within the `MainActivity`, allowing it to receive dependencies provided by Hilt modules.

--- app/src/main/java/com/bassem/redditnews/MainActivity.kt
+++ app/src/main/java/com/bassem/redditnews/MainActivity.kt
@@ -7,12 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.bassem.navigation.NavigationC
 import com.bassem.redditnews.ui.theme.RedditNewsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
